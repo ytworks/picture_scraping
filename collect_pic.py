8807bd2dddcb5e9ddc45e9bde620aa292dde83af
ytworks <yuzo.takagi@healthdataplatform.co.jp>
2016-10-21 15:21:11 +0900

    release

--- collect_pic.py
+++ collect_pic.py
@@ -4,29 +4,32 @@
 import os
 import requests
 from pyquery import PyQuery as pq
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
+def main(page, scroll = 100):
+    browser = webdriver.Chrome()
+    browser.get(page)
+    for i in range(100):
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-
-def main(page):
     save_path = os.path.abspath('.')
-    r = requests.get(page)
-    query = pq(r.content)
-    #imgs_html = query('img').filter('.btnOver')
-    imgs_html = query('img')
-
     download_count = 0
-    for img_html in imgs_html:
-        img_file = pq(img_html).attr('src')
-        img_url = img_file
+    for img in browser.find_elements_by_tag_name("img"):
+        if img.get_attribute('src') != None:
+            if img.get_attribute('src').find('gstatic.com/images?q=tbn:') > 0:
+                img_url = img.get_attribute('src')
+                output = '%s/%03d.jpg' % (save_path,download_count)
+                print('%s : Download... %s' % (download_count, os.path.basename(img_url)))
+                with open(output,'wb') as f:
+                    raw = requests.get(img_url).content
+                    f.write(raw)
+                download_count += 1
 
-        download_count += 1
-        output = '%s/%03d.jpg' % (save_path,download_count)
-        print('%s : Download... %s' % (download_count, os.path.basename(img_url)))
-        with open(output,'wb') as f:
-            raw = requests.get(img_url).content
-            f.write(raw)
+    browser.close()
+
 
 if __name__ == '__main__':
     page = "https://www.google.co.jp/search?q=%E3%82%A4%E3%83%B3%E3%83%91%E3%83%8D&espv=2&biw=1110&bih=536&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjs-L-2-OjPAhUp9IMKHVppCLUQ_AUIBigB#imgrc=_"
-    main(page = page)
+    main(page = page, scroll = 100)
